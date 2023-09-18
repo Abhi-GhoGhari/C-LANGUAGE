87f9d2b706838a895da2d571a0449f52915321d0
AbhiGhoGhari <abhighoghari8182@gmail.com>
2023-09-18 10:44:33 +0530

    17/09/23

---
+++ PR-2/Q-1.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	char AG;
+	clrscr();
+	printf("ENTER THE VALUE= ");
+	scanf("%c",&AG);
+
+	if( (AG>='A'&& AG<='Z')||(AG>='a' && AG<='z') )
+	{
+		printf("!! IT'S ALPHABETS !!");
+	}
+	else if(AG>='0' && AG<='9')
+	{
+		printf("!! IT'S NUMBER !!");
+	}
+	else
+	{
+		printf("IT'S SPECIAL CHAREACTER");
+	}
+	getch();
+}
---
+++ PR-2/Q-2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	char A;
+	clrscr();
+	printf("ENTER ANY NUMBER= ");
+	scanf("%i",&A);
+	(a%2==0)
+		? ITS EVEN
+		: ITS ODD;3
+	getch();
+}
