31813b7b966c1fe69b94aa8aeb2e79207cdd9416
ABHI GHOGHARI <143987211+Abhi-GhoGhari@users.noreply.github.com>
2023-10-24 19:01:44 +0530

    Add files via upload

---
+++ PR-6/Q-3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char string1, string2;
+    int len1 = 0, len2 = 0, com = 0,i;
+
+    printf("ENTER FIRST STRING= ");
+    scanf("%[^\n]", &string1);
+    printf("ENTER SECOND STRING= ");
+    scanf("%[^\n]", &string2);
+
+    for (i = 0; string1 != NULL; i++)
+        len1++;
+    for (i = 0; string2 != NULL; i++)
+        len2++;
+
+    if (len1 == len2)
+    {
+        for (i = 0; i < len1; i++)
+        {
+            if (string1 == string2)
+                com++;
+        }
+
+        (len1 == com)
+            ? printf("GIVEN STRING IS SAME!!")
+            : printf("GIVEN STRING IS NOT SAME !!");
+    }
+    else
+        printf("GIVEN STRING IS NOT SAME !!");
+}
