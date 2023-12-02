a9bb5d5be864e2861b51c70ef82cf8c24128de20
ABHI GHOGHARI <143987211+Abhi-GhoGhari@users.noreply.github.com>
2023-12-02 12:11:06 +0530

    Add files via upload

---
+++ ch-13/Q-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *read, *write;
+    char data[50], value[50];
+
+    read = fopen("1st.txt", "r");
+    write = fopen("2nd.txt", "w");
+
+    if (read != NULL)
+    {
+        while (fgets(data, 2, read) != NULL)
+        {
+            printf("%s", data);
+            fprintf(write, "%s", data);
+        }
+    }
+}
